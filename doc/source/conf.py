66afaba6ed026264e8ae8c4116bf07060b3b12a3
saullocastro <saullogiovani@gmail.com>
2016-03-12 08:30:33 -0300

    DOC: updated authors

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'structmanager'
-copyright = u'2015, Saullo Castro'
+copyright = u'2015-2016, Saullo Castro and Guilherme Abreu'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -217,7 +217,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'structmanager.tex', u'structmanager Documentation',
-   u'Saullo Castro', 'manual'),
+   u'Saullo Castro and Guilherme Abreu', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -247,7 +247,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'structmanager', u'structmanager Documentation',
-     [u'Saullo Castro'], 1)
+     [u'Saullo Castro and Guilherme Abreu'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -282,9 +282,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = u'structmanager'
-epub_author = u'Saullo Castro'
-epub_publisher = u'Saullo Castro'
-epub_copyright = u'2015, Saullo Castro'
+epub_author = u'Saullo Castro and Guilherme Abreu'
+epub_publisher = u'Saullo Castro and Guilherme Abreu'
+epub_copyright = u'2015-2016, Saullo Castro and Guilherme Abreu'
 
 # The basename for the epub file. It defaults to the project name.
 #epub_basename = u'structmanager'
