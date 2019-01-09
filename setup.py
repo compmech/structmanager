c16b3877c8a124a0024f15423188c873be00d698
saullocastro <saullogiovani@gmail.com>
2019-01-10 00:25:53 +0100

    BLD: updated setup to newer standards

--- setup.py
+++ setup.py
@@ -1,88 +1,15 @@
-#!/usr/bin/env python
-"""StructManager: Structural Manager for Python
+from __future__ import absolute_import
 
-Structural Manager is intented to help in GFEM-type of analysis, where the
-finite elment discretization is at a global level and detailed analysis is
-performed using analytical or semi-analytical methods.
-
-The structure is mapped onto the finite element model using two mapping files
-and additional data is usually required to define for example exact beam
-profiles that will be used in the real structures.
-
-This setup is based on SciPy's setup.
-
-"""
 import os
-import sys
+import inspect
 import subprocess
-
-if sys.version_info[:2] < (2, 6) or (3, 0) <= sys.version_info[0:2] < (3, 2):
-    raise RuntimeError("Python version 2.7 or >= 3.2 required.")
-
-if sys.version_info[0] < 3:
-    import __builtin__ as builtins
-else:
-    import builtins
-
-DOCLINES = __doc__.split("\n")
-
-CLASSIFIERS = """\
-Intended Audience :: Stress Analysis Engineers
-License :: BSD Approved
-Programming Language :: FORTRAN
-Programming Language :: C
-Programming Language :: C++
-Programming Language :: Python
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: Microsoft :: Windows
-Operating System :: Unix
-
-"""
-
-MAJOR = 0
-MINOR = 4
-MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+from setuptools import setup, find_packages
 
 
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
+is_released = True
+version = '0.4.0'
 
 
-
-def write_version_py(filename='structmanager/version.py'):
-    cnt = """
-# THIS FILE IS GENERATED FROM StructManager setup.py
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-isreleased = %(isreleased)s
-if isreleased:
-    __version__ = version
-else:
-    __version__ = full_version
-
-if not isreleased:
-    version = full_version
-"""
-    FULLVERSION, GIT_REVISION = get_version_info()
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'version': VERSION,
-                       'full_version': FULLVERSION,
-                       'git_revision': GIT_REVISION,
-                       'isreleased': str(ISRELEASED)})
-    finally:
-        a.close()
-
-
-# Return the git revision as a string
 def git_version():
     def _minimal_ext_cmd(cmd):
         # construct minimal environment
@@ -100,99 +27,88 @@ def git_version():
 
     try:
         out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
+        git_revision = out.strip().decode('ascii')
     except OSError:
-        GIT_REVISION = "Unknown"
+        git_revision = "Unknown"
 
-    return GIT_REVISION
-
-
-def get_version_info():
-    FULLVERSION = VERSION
-    GIT_REVISION = git_version()
-    if not ISRELEASED:
-        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
-
-    return FULLVERSION, GIT_REVISION
+    return git_revision
 
 
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-
-    config.add_subpackage('structmanager')
-    config.add_data_files(('structmanager', '*.txt'))
-
-    config.get_version('structmanager/version.py')
-
-    return config
+def get_version_info(version, is_released):
+    fullversion = version
+    if not is_released:
+        git_revision = git_version()
+        fullversion += '.dev0+' + git_revision[:7]
+    return fullversion
 
 
-def setup_package():
-    cmdclass = {}
-    # Figure out whether to add ``*_requires = ['numpy']``.
-    # We don't want to do that unconditionally, because we risk updating
-    # an installed numpy which fails too often.  Just if it's not installed, we
-    # may give it a try.  See gh-3379.
-    build_requires = ['numpy>=1.10.4', 'scipy>=0.15.0']
-
-    FULLVERSION, GIT_REVISION = get_version_info()
-
-    metadata = dict(
-        name='structmanager',
-        maintainer='StructManager Developers',
-        maintainer_email='saullo.castro@embraer.com.br ',
-        version=FULLVERSION,
-        description=DOCLINES[0],
-        long_description='\n'.join(DOCLINES[2:]),
-        url='http://structmanager.github.io/structmanager/',
-        download_url='https://github.com/structmanager/structmanager/releases',
-        license='BSD',
-        cmdclass=cmdclass,
-        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-        platforms=['Windows', 'Linux'],
-        #test_suite='nose.collector',
-        setup_requires=build_requires,
-        install_requires=build_requires,
-    )
+def write_version_py(version, is_released, filename='structmanager/version.py'):
+    fullversion = get_version_info(version, is_released)
+    with open("./structmanager/version.py", "wb") as f:
+        f.write(b'__version__ = "%s"\n' % fullversion.encode())
+    return fullversion
 
 
-    if len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
-            sys.argv[1] in ('--help-commands', 'egg_info', '--version',
-                            'clean')):
-        # For these actions, NumPy is not required.
-        #
-        # They are required to succeed without Numpy for example when
-        # pip is used to install Scipy when Numpy is not yet present in
-        # the system.
-        try:
-            from setuptools import setup
-        except ImportError:
-            from distutils.core import setup
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    setupdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return open(os.path.join(setupdir, fname)).read()
 
-        FULLVERSION, GIT_REVISION = get_version_info()
-        metadata['version'] = FULLVERSION
-    else:
-        if (len(sys.argv) >= 2 and sys.argv[1] in ('bdist_wheel', 'bdist_egg')) or (
-                    'develop' in sys.argv):
-            # bdist_wheel/bdist_egg needs setuptools
-            import setuptools
 
-        from numpy.distutils.core import setup
+#_____________________________________________________________________________
 
-        metadata['configuration'] = configuration
+install_requires = [
+        "numpy",
+        "scipy",
+        "coveralls",
+        "pyNastran",
+        "setuptools-git-version",
+        ]
 
-    setup(**metadata)
+CLASSIFIERS = """\
 
-if __name__ == '__main__':
-    write_version_py()
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+Intended Audience :: Education
+Topic :: Scientific/Engineering :: Mathematics
+License :: OSI Approved :: BSD License
+Operating System :: Microsoft :: Windows
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.5
+Operating System :: Unix
 
-    if os.name == 'nt' and os.environ.get('CONDA_DEFAULT_ENV') is None:
-        os.environ['DISTUTILS_USE_SDK'] = '1'
-        os.environ['MSSdk'] = '1'
+"""
 
-    setup_package()
+fullversion = write_version_py(version, is_released)
+
+data_files = [('', [
+        'README.rst',
+        'LICENSE',
+        'structmanager/version.py',
+        ])]
+
+package_data = {
+        '': ['tests/*.*'],
+        }
+
+s = setup(
+    name = "structmanager",
+    version = fullversion,
+    author = "Saullo G. P. Castro",
+    author_email = "castrosaullo@gmail.com",
+    description = ("Structural Analysis Helper"),
+    license = "BSD",
+    keywords = "structural analysis; stress analysis; automation",
+    url = "https://github.com/compmech/structmanager",
+    packages=find_packages(),
+    package_data=package_data,
+    data_files=data_files,
+    long_description=read('README.rst'),
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    install_requires=install_requires,
+)
+
